5c6654824fd637eb10d8897d41423c1d54cbd73d
Nikola Ivanov <nikola4421@gmail.com>
2019-07-21 18:34:43 +0300

    Fixing up ESLint rules

--- src/WorkManager.Client/.eslintrc.js
+++ src/WorkManager.Client/.eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   extends: [
     "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
     "plugin:@typescript-eslint/recommended", // Uses the recommended rules from @typescript-eslint/eslint-plugin,
-    "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict withprettier
   ],
   root: true, // if false, eslint looks up in folders for other .eslintrc files and merges them into one
   parserOptions: {
@@ -16,6 +16,9 @@ module.exports = {
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-empty-interface": "off",
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/array-type": "off"
   },
   settings: {
     react: {
