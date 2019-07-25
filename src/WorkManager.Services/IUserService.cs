121045eee91f6833a74c4bb7d19bc68a1cdd7e57
zenVentzi <v.marinov96@gmail.com>
2019-07-25 09:30:14 +0300

    Add WorkManager.Services project

---
+++ src/WorkManager.Services/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using WorkManager.Data.Models;
+
+namespace WorkManager.Services
+{
+    public interface IUserService
+    {
+        // we need infrastructure project(view models) to type proper types
+    }
+}
