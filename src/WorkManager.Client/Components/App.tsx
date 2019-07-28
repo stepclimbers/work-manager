5fc1f760d81253211b29ae3db1c6679fe4ab65bd
Nikola Ivanov <nikola4421@gmail.com>
2019-07-28 21:27:46 +0300

    Extracting header into a component

--- src/WorkManager.Client/Components/App.tsx
+++ src/WorkManager.Client/Components/App.tsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Register from './user/Register';
 import Login from './user/Login';
-import { Layout, Menu } from 'antd';
+import Header from './navigation/Header';
+import { Layout } from 'antd';
 
-const { Header, Content, Footer } = Layout;
+const { Content, Footer } = Layout;
 
 interface IAppProps {}
 interface IAppState {
@@ -30,21 +31,7 @@ class App extends Component<Readonly<IAppProps>, IAppState> {
         return (
             <Router>
                 <Layout className="layout">
-                    <Header>
-                        <div className="logo" />
-                        <Menu
-                            theme="dark"
-                            mode="horizontal"
-                            defaultSelectedKeys={['1']}
-                            style={{ lineHeight: '64px' }}>
-                            <Menu.Item key="1">
-                                <Link to="/user/register">Register</Link>
-                            </Menu.Item>
-                            <Menu.Item key="2">
-                                <Link to="/user/login">Login</Link>
-                            </Menu.Item>
-                        </Menu>
-                    </Header>
+                    <Header />
                     <Content style={{ padding: '0 50px' }}>
                         <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
                             <Route path="/user/register" exact component={Register} />
