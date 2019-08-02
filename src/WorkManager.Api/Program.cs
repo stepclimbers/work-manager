26b3a4b3477bf6155e5665dec71978e16ca2ef72
Nikola Ivanov <nikola4421@gmail.com>
2019-08-02 18:43:26 +0300

    Adding logging setup in Program.cs

--- src/WorkManager.Api/Program.cs
+++ src/WorkManager.Api/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace WorkManager.Api
 {
@@ -12,6 +13,13 @@ namespace WorkManager.Api
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    logging.AddConsole();
+                    logging.AddDebug();
+                    logging.AddEventSourceLogger();
+                });
     }
 }
