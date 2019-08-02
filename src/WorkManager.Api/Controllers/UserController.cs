3404c3777997f97a792df99375f920cdb31464f3
Nikola Ivanov <nikola4421@gmail.com>
2019-08-02 18:43:43 +0300

    Adding logging for user controller

--- src/WorkManager.Api/Controllers/UserController.cs
+++ src/WorkManager.Api/Controllers/UserController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using WorkManager.Core.Exceptions;
 using WorkManager.Core.ViewModels;
 using WorkManager.Core.ViewModels.Authorize;
@@ -14,25 +15,30 @@ namespace WorkManager.Api.Controllers
     public class UserController : CustomBaseController
     {
         private readonly IUserService userService;
+        private readonly ILogger logger;
 
-        public UserController(IUserService userService)
+        public UserController(IUserService userService, ILogger<UserController> logger)
         {
             this.userService = userService;
+            this.logger = logger;
         }
 
         [HttpPost("[action]")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(Microsoft.AspNetCore.Mvc.ModelBinding.ModelStateDictionary), StatusCodes.Status400BadRequest)]
         [ProducesDefaultResponseType]
+        [AllowAnonymous]
         public async Task<IActionResult> Register([FromBody] RegisterModel model)
         {
             var result = await this.userService.RegisterUserAsync(model);
             if (result.Succeeded)
             {
+                this.logger.LogInformation($"User {model.Email} has registered.");
                 return this.Ok();
             }
 
             this.AddErrors(result);
+            this.logger.LogWarning($"User {model.Email} has failed registration: ", this.ModelState);
 
             return this.BadRequest(this.ModelState);
         }
@@ -47,10 +53,13 @@ namespace WorkManager.Api.Controllers
             try
             {
                 var result = await this.userService.AuthenticateUserAsync(model);
+
+                this.logger.LogInformation($"User {model.Email} has been authenticated.");
                 return this.Ok(result);
             }
             catch (UserAuthenticationException)
             {
+                this.logger.LogInformation($"User {model.Email} has failed authentication.");
                 return this.Unauthorized();
             }
         }
