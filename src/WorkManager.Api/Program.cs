7d9a5a35abd05f038a04cc285f816c610a6b374e
Nikola Ivanov <nikola4421@gmail.com>
2019-08-04 11:58:43 +0300

    Adding configuration for loggly

--- src/WorkManager.Api/Program.cs
+++ src/WorkManager.Api/Program.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.AspNetCore;
+﻿using Loggly;
+using Loggly.Config;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Serilog;
+using WorkManager.Core.Settings;
 
 namespace WorkManager.Api
 {
@@ -21,7 +25,31 @@ namespace WorkManager.Api
                 })
                 .UseSerilog((hostingContext, loggerConfiguration) =>
                 {
+                    var logglySettings = hostingContext.Configuration.GetSection(nameof(LogglySettings)).Get<LogglySettings>();
+                    SetupLogglyConfiguration(logglySettings);
+
                     loggerConfiguration.ReadFrom.Configuration(hostingContext.Configuration);
                 });
+
+        private static void SetupLogglyConfiguration(LogglySettings logglySettings)
+        {
+            // Configure Loggly
+            var config = LogglyConfig.Instance;
+            config.CustomerToken = logglySettings.CustomerToken;
+            config.ApplicationName = logglySettings.ApplicationName;
+            config.Transport = new TransportConfiguration()
+            {
+                EndpointHostname = logglySettings.EndpointHostname,
+                EndpointPort = logglySettings.EndpointPort,
+                LogTransport = logglySettings.LogTransport
+            };
+            config.ThrowExceptions = logglySettings.ThrowExceptions;
+
+            // Define Tags sent to Loggly
+            config.TagConfig.Tags.AddRange(new ITag[]{
+                new ApplicationNameTag {Formatter = "Application-{0}"},
+                new HostnameTag { Formatter = "Host-{0}" }
+            });
+        }
     }
 }
